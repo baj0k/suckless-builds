d00c9a4e38d195e30aa98d60224e134cdf8f0a23
bajok <bajok@kretes.xyz>
2022-05-03 03:56:10 +0200

    Add blocks from i3, finalize migration

--- dwmblocks/blocks.h
+++ dwmblocks/blocks.h
@@ -1,9 +1,17 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
-	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"",		"sb-clock",		60,					1},
-	{"🌏",		"sb-openvpn",	0,					11},
+	/*Icon*/	/*Command*/			/*Update Interval*/	/*Update Signal*/
+	{"",		"sb-weather",		1800,				14 },
+	{"",		"sb-brightness",	0,					13 },
+	{"🔉",		"sb-audio",			0,					12 },
+	{"🌏",		"sb-openvpn",		0,					11 },
+	{"📡",		"sb-ip",			0,					10 },
+	{"🎵",		"sb-music",			10,					9  },
+	{"💻",		"sb-memory",		10,					4  },
+	{"🖴",		"sb-disk",			60,					3  },
+	{"🕓",		"sb-clock",			1,					2  },
+	{"🤔",		"sb-update",		1800,				1  },
 };
 
 //Sets delimiter between status commands. NULL character ('\0') means no delimiter.
-static char *delim = "|";
+static char *delim = " ";
